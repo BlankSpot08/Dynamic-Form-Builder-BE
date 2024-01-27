31b08265d253796d43ac00b80cd02f213ef79194
Arvin <arvin@comgtech.com>
2024-01-27 15:08:30 +0800

    cleaning code

--- config/env/env.config.env.js
+++ config/env/env.config.env.js
@@ -2,6 +2,6 @@ import development from './development.configs.env.js'
 
 const env = process.env.NODE_ENV || 'development';
 const environments = { development };
-
 const currentEnv = environments[env]
+
 export default currentEnv;

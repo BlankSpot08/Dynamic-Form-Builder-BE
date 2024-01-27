452f1d12792f79ff73aae4d4a2ed6485f2cf511a
Arvin <arvin@comgtech.com>
2024-01-27 14:41:09 +0800

    added MONGODB_URI property

--- config/env/development.configs.env.js
+++ config/env/development.configs.env.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const env = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    MONGODB_URI: process.env.MONGODB_URI
 };
 
 export default env;

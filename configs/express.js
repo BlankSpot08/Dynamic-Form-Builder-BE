b0d1d889eadfade1c26d58de448aa7c6873f7c8a
Arvin <arvin@comgtech.com>
2024-01-27 12:27:01 +0800

    removed cors and added an export default

--- configs/express.js
+++ configs/express.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import cors from 'cors'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
 
 // app.use('/api', routes)
 
-module.exports = app;
+export default app

7fa7e99f0a329b83ce757ff5af22bbdef9da1c36
Arvin <arvin@comgtech.com>
2024-01-27 13:09:06 +0800

    added the routes

--- configs/express.configs.js
+++ configs/express.configs.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from '../app/routes/index.routes'
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use('/api', routes)
+app.use('/api', routes)
 
 export default app;

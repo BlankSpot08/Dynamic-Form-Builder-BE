b214790589d2c00af107c9d7471bba50f2de1a61
Arvin <arvin@comgtech.com>
2024-01-29 19:30:22 +0800

    added a title parameter for getByTitle

--- app/routes/form.routes.js
+++ app/routes/form.routes.js
@@ -4,7 +4,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/get-all', getAll);
-router.get('/get', getByTitle);
+router.get('/get/:title', getByTitle);
 router.post('/create', create);
 router.put('/update', update);
     

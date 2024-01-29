e478edcdeadf13ef1266e918caf0c69c69091579
Arvin <arvin@comgtech.com>
2024-01-29 19:21:17 +0800

    added the catch of getAll completed the getByTitle completed the create and added the catch of the update function

--- app/repositories/form.repository.js
+++ app/repositories/form.repository.js
@@ -1,4 +1,4 @@
-import Form from "../models/form.model"
+import Form from "../models/form.model.js"
 
 export async function getAll() {
     try {
@@ -9,26 +9,63 @@ export async function getAll() {
     
     catch(error) {
         console.error('Repository error:', error.message);
+    }
+}
+
+export async function getByTitle(title) {
+    try {
+        const form = await Form.find({ title: title });
+
+        return form
+    } 
+    
+    catch(error) {
+        console.error('Repository error:', error.message);
         throw new Error('Error retrieving forms from the database');
     }
 }
 
-export async function get() {
+export async function create(fields) {
     try {
+        const title = fields[0]
 
+        const newFields = []
+        for (let i = 1; i < fields.length; i++) {
+            const currentField = fields[i];
+            newFields.push({
+                "label": currentField.inputLabel,
+                "placeholder": currentField.inputPlaceholder,
+                "answer": currentField.inputAnswer,
+                "type": currentField.inputType,
+            })
+
+            if (Object.hasOwn(currentField, "choices")) {
+                newFields[newFields.length - 1]["choices"] = currentField.choices;
+            }
+        }
+                
+        const form = new Form({
+            title: title.inputTitle,
+            description: title.inputDescription,
+            fields: newFields
+        });
+
+        await form.save();
     } 
     
     catch(error) {
-
+        console.error('Repository error:', error.message);
+        throw new Error('Error retrieving forms from the database');
     }
 }
 
 export async function update() {
     try {
-
+        return 'forms'
     } 
     
     catch(error) {
-
+        console.error('Repository error:', error.message);
+        throw new Error('Error retrieving forms from the database');
     }
 }

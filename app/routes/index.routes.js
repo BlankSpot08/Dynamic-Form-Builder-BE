df4505788393ee0e41190a9f36842b2e4e8af363
Arvin <arvin@comgtech.com>
2024-01-30 22:13:33 +0800

    added submission route

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -2,7 +2,9 @@ import express from 'express'
 const router = express.Router()
 
 import formRoutes from './form.routes.js'
+import submissionRoutes from './submission.routes.js'
 
 router.use('/form', formRoutes)
+router.use('/submission', submissionRoutes)
 
 export default router
